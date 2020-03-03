952862c1e04ccf7aac62529259c128b2328cbacd
Loi Tran <33661324+tdloi@users.noreply.github.com>
2020-03-04 00:04:41 +0700

    Update regex code desktop

--- scripts/desktop/code.sh
+++ scripts/desktop/code.sh
@@ -1,4 +1,4 @@
 # group subtitle group with ()
 # -r to toggle --regexp-extended options
-sudo sed -ri 's/.*Exec=code-oss(.*)/Exec=code-oss --extensions-dir .local\/share\/vscode-oss\/extensions \1/g' /usr/share/applications/code-oss.desktop
+sudo sed -ri 's/.*Exec=\/usr\/bin\/code-oss(.*)/Exec=\/usr\/bin\/code-oss --extensions-dir .local\/share\/vscode-oss\/extensions \1/g' /usr/share/applications/code-oss.desktop
 echo -e "\033[0;32mDONE!\033[0m"
